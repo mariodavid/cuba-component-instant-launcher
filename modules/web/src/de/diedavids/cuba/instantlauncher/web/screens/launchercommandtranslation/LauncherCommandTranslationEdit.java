2a8ddfca2719526d602980e5ceb7f7a45ec35ef4
Mario <mario@die-davids.de>
2019-11-25 07:50:20 +0100

    switch beanLauncher and translations to CUBA 7 UI API

--- modules/web/src/de/diedavids/cuba/instantlauncher/web/screens/launchercommandtranslation/LauncherCommandTranslationEdit.java
+++ modules/web/src/de/diedavids/cuba/instantlauncher/web/screens/launchercommandtranslation/LauncherCommandTranslationEdit.java
@@ -6,37 +6,35 @@ import com.haulmont.cuba.gui.components.FieldGroup;
 import com.haulmont.cuba.gui.components.FieldGroup.FieldConfig;
 import com.haulmont.cuba.gui.components.LookupField;
 import com.haulmont.cuba.gui.data.Datasource;
+import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
 import de.diedavids.cuba.instantlauncher.entity.LauncherCommandTranslation;
+
+import java.util.Locale;
 import java.util.Map;
 import javax.inject.Inject;
 
-public class LauncherCommandTranslationEdit extends AbstractEditor<LauncherCommandTranslation> {
+import com.haulmont.cuba.gui.screen.*;
+import de.diedavids.cuba.instantlauncher.entity.LauncherCommandTranslation;
 
-  @Inject
-  protected FieldGroup fieldGroup;
+import com.haulmont.cuba.gui.screen.*;
+import de.diedavids.cuba.instantlauncher.entity.LauncherCommandTranslation;
 
-  @Inject
-  protected ComponentsFactory componentsFactory;
-
-
-  @Inject
-  protected Datasource<LauncherCommandTranslation> launcherCommandTranslationDs;
-
+@UiController("ddcil$LauncherCommandTranslation.edit")
+@UiDescriptor("launcher-command-translation-edit.xml")
+@EditedEntityContainer("launcherCommandTranslationDc")
+@LoadDataBeforeShow
+public class LauncherCommandTranslationEdit extends StandardEditor<LauncherCommandTranslation> {
 
   @Inject
   protected GlobalConfig globalConfig;
+  @Inject
+  protected LookupField<Locale> localeField;
 
-  @Override
-  public void init(Map<String, Object> params) {
-    super.init(params);
-
-    FieldConfig fieldConfig = fieldGroup.getField("locale");
-    LookupField localeField = componentsFactory.createComponent(LookupField.class);
-    localeField.setOptionsMap(globalConfig.getAvailableLocales());
-    localeField.setDatasource(launcherCommandTranslationDs, "locale");
-    fieldConfig.setComponent(localeField);
-
+  @Subscribe
+  protected void onInit(InitEvent event) {
+    localeField.setOptionsMap(
+            globalConfig.getAvailableLocales()
+    );
   }
-
 }
